4926c89fad47ec2af1d673f08c5afc08348babc2
Nikita Volobuev <nikitavbv@gmail.com>
2022-01-06 15:27:12 +0200

    feat(server): add websocket

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,9 +1,19 @@
 package com.nikitavbv.disaster
 
 import notifcenter.{EonetNotificationCenter, PdcNotificationCenter}
+import model.DisasterFormat._
 
+import akka.NotUsed
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.Directives.{concat, get, getFromResource, getFromResourceDirectory, handleWebSocketMessages, path, pathSingleSlash}
+import akka.http.scaladsl.{Http, server}
+import akka.stream.Attributes
+import akka.stream.scaladsl.{Flow, Source}
 import com.nikitavbv.disaster.calendar.GoogleCalendarClient
+import com.nikitavbv.disaster.model.{Disaster, DisasterLocation}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -11,17 +21,38 @@ object Application {
   implicit val system: ActorSystem = ActorSystem("DisasterAdviser")
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  def main(args: Array[String]) = {
-   /*Seq(
-      EonetNotificationCenter.monitorEvents().map(_.toDisaster),
-      PdcNotificationCenter.monitorEvents().map(_.toDisaster)
-    ).reduce((a, b) => a merge b).runForeach(println)*/
+  var disasterSource: Source[Disaster, NotUsed] = Seq(
+    EonetNotificationCenter.monitorEvents().map(_.toDisaster),
+    PdcNotificationCenter.monitorEvents().map(_.toDisaster)
+  ).reduce((a, b) => a merge b)
 
-    new GoogleCalendarClient(sys.env("GOOGLE_CALENDAR_TOKEN"))
+  val port = 8080
+
+  val routes: server.Route = get {
+    concat(
+      path("disasters") {
+        handleWebSocketMessages(handleWebsocket())
+      },
+      get {
+        getFromResourceDirectory("ui")
+      }
+    )
+  }
+
+  def handleWebsocket(): Flow[Message, Message, Any] = {
+    Flow[Message]
+      .merge(disasterSource.map(v => TextMessage(v.toJson.toString)))
+      .merge(Source.single(TextMessage("Hello!")))
+  }
+
+  Http().newServerAt("0.0.0.0", port).bindFlow(routes)
+
+  def main(args: Array[String]) = {
+    /*new GoogleCalendarClient(sys.env("GOOGLE_CALENDAR_TOKEN"))
       .allEvents
       .runForeach(println)
-      .onComplete(println)
+      .onComplete(println)*/
 
-    println("Hello, World!")
+    println(s"server started at port ${port}")
   }
 }
--- src/main/scala/model/Disaster.scala
+++ src/main/scala/model/Disaster.scala
@@ -1,9 +1,18 @@
 package com.nikitavbv.disaster
 package model
 
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
 case class Disaster(id: String,
                     title: String,
                     location: Seq[DisasterLocation])
 
 case class DisasterLocation(latitude: Double,
                             longitude: Double)
+
+object DisasterFormat {
+
+  implicit val locationFormat: RootJsonFormat[DisasterLocation] = jsonFormat2(DisasterLocation)
+  implicit val disasterFormat: RootJsonFormat[Disaster] = jsonFormat3(Disaster)
+}
