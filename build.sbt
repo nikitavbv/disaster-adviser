d4ce120f29cb3a881c04324e2f92f925078f1ad4
Nikita Volobuev <nikitavbv@gmail.com>
2022-01-06 02:07:37 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "disaster-adviser-v2"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
+
+idePackagePrefix := Some("com.nikitavbv.disaster")
---
+++ src/main/scala/Application.scala
@@ -0,0 +1,8 @@
+package com.nikitavbv.disaster
+
+object Application {
+
+  def main(args: Array[String]) = {
+    println("Hello, World!")
+  }
+}
